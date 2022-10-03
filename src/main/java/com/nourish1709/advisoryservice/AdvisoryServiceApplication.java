7253f9d0d9d153dc5530a092b86594070ee60d18
Yevhenii Savonenko <yevs@jacobianengineering.com>
2022-10-03 09:26:25 +0300

    Initial commit

---
+++ src/main/java/com/nourish1709/advisoryservice/AdvisoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nourish1709.advisoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvisoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvisoryServiceApplication.class, args);
+    }
+
+}
